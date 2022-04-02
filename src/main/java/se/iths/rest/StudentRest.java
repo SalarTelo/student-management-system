0e45a78e81567c171d3579c98f891a2113f27419
salar <salarmail12@gmail.com>
2022-04-02 21:07:12 +0200

    init

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,66 @@
 package se.iths.rest;
 
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("students")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentRest {
 
+    StudentService service;
+
+    @Inject
+    public StudentRest(StudentService service){
+        this.service = service;
+    }
+
+    @Path("")
+    @POST
+    public Response createStudent(Student data){
+        service.createStudent(data);
+        return Response.ok(data).build();
+    }
+
+    @Path("")
+    @PUT
+    public Response updateStudent(Student data){
+        service.updateStudent(data);
+        return Response.ok(data).build();
+    }
+
+    @Path("{id}")
+    @GET
+    public Response findStudentById(@PathParam("id") Long id){
+        Student student = service.findStudentById(id);
+        if (student == null) {
+
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+                    .entity("Student with ID " + id + " was not found in database.").type(MediaType.TEXT_PLAIN_TYPE).build());
+        }
+        return Response.ok(student).build();
+    }
+
+    @Path("")
+    @GET
+    public Response getAllStudents(){
+        List<Student> students = service.getAllStudents();
+        return Response.ok(students).build();
+    }
+
+
+    @Path("{id}")
+    @DELETE
+    public Response deleteStudent(@PathParam("id") Long id){
+        service.deleteStudent(id);
+        return Response.ok().build();
+    }
 
 }
--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,37 @@
 package se.iths.service;
 
+import se.iths.entity.Student;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
 public class StudentService {
 
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public void createStudent(Student data){
+        entityManager.persist(data);
+    }
+
+    public void updateStudent(Student data){
+        entityManager.merge(data);
+    }
+
+    public Student findStudentById(Long id){
+        return entityManager.find(Student.class, id);
+    }
+
+    public List<Student> getAllStudents(){
+        return entityManager.createQuery("SELECT i from Student i", Student.class).getResultList();
+    }
+
+    public void deleteStudent(Long id){
+        Student student = entityManager.find(Student.class, id);
+        entityManager.remove(student);
+    }
 
 }
